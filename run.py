590c27e04e87d71ad68cb725da85bc7f50270beb
bartekburnat7 <>
2023-08-11 20:09:33 +0000

    Add main function with two inputs with your name and your yearly income

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    user_name = input("Enter your Name: ")
+    user_earn = input("How much you earn a year: £")
+
+main()
