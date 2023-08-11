741f744e8ec46cffff2654ad761f10c597624664
bartekburnat7 <>
2023-08-11 20:22:37 +0000

    Add a function that finds your tax band and then calculates your tax accordingly

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
+def tax_calculate(x):
+    print("Calculating....")
+    if x <= 12570:
+        tax_pay = 0
+        return tax_pay
+
+    elif x >= 12571 and x <= 50270:
+        tax_pay = 0.2 * (x - 12570)
+        return tax_pay
+
+    elif x >= 50271 and x <= 125140:
+        tax_pay = 7539.8 + 0.4 * (x - 50271)
+        return tax_pay
+
+    elif x > 125140:
+        tax_pay = 29947.6 + 0.45 * (x - 125140)
+        return tax_pay
+
 def main():
     user_name = input("Enter your Name: ")
     user_earn = input("How much you earn a year: £")
