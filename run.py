57ed2344e47aeff68013eb392fdd95a1db3f1b68
bartekburnat7 <>
2023-11-08 12:10:20 +0000

    update validator errors and added comments

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-def tax_calculate(x):
+# This function takes the input from "user_earn"
+# and checks which tax band the user would qualify
+# in and then calculates the tax accordingly.
+def calculate(x):
     print("\nCalculating....\n")
     if x <= 12570:
         print("--- Personal Allowance Band ---")
@@ -20,24 +23,36 @@ def tax_calculate(x):
         tax_pay = (7540 + 29948) + 0.45 * (x - 125140)
         return tax_pay
 
+# Main loop for the application which
+# takes the user input and displays the
+# amount of tax you would need to pay.
+
 
 def main():
     print("-------------------------------------------")
     print("    WELCOME TO THE TAX CALCULATOR (UK)")
     print("APPLICATION THAT CALCULATES YOUR ANNUAL TAX")
     print("-------------------------------------------")
-    user_name = input("Please enter your Name: ")
+
+    name = input("Please enter your Name: ")
+    text_one = ", you would be paying: £"
+    text_two = "TAX this year"
     while True:
         user_earn = input("How much you earn a year: £")
-
+        # This is an input handler. If the value is a float or
+        # a numerical number it will accept it. But if a letter
+        # or special character has been entered then the
+        # application would not accept it and return an error message.
         try:
             float()
             user_earn = float(user_earn)
-            print(user_name , ", you would be paying: £" , "%.2f" % tax_calculate(user_earn) , "TAX this year")
+            print(name, text_one, "%.2f" % calculate(user_earn), text_two)
         except ValueError:
             if user_earn.isnumeric():
                 user_earn = int(user_earn)
-                print(user_name , ", you would be paying: £", "%.2f" % tax_calculate(user_earn) , "TAX this year")
+                print(name, text_one, "%.2f" % calculate(user_earn), text_two)
             else:
                 print("Insert a correct value to calculate your annual Tax!!!")
+
+
 main()
