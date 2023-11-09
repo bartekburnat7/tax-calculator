f84822f6ab813b6560e2e62acd76183deac0ecf0
bartekburnat7 <>
2023-11-09 15:14:11 +0000

    devide the main function into smaller functions and edited some virable names.

--- run.py
+++ run.py
@@ -1,58 +1,65 @@
 # This function takes the input from "user_earn"
 # and checks which tax band the user would qualify
 # in and then calculates the tax accordingly.
-def calculate(x):
+def calculate(user_earnings):
     print("\nCalculating....\n")
-    if x <= 12570:
+    if user_earnings <= 12570:
         print("--- Personal Allowance Band ---")
-        tax_pay = 0
-        return tax_pay
-
-    elif x >= 12571 and x <= 50270:
+        tax_payable = 0
+    elif user_earnings >= 12571 and user_earnings <= 50270:
         print("--- Basic rate Band ---")
-        tax_pay = 0.2 * (x - 12570)
-        return tax_pay
-
-    elif x >= 50271 and x <= 125140:
+        tax_payable = 0.2 * (user_earnings - 12570)
+    elif user_earnings >= 50271 and user_earnings <= 125140:
         print("--- Higher rate Band ---")
-        tax_pay = 7540 + 0.4 * (x - 50270)
-        return tax_pay
-
-    elif x > 125140:
+        tax_payable = 7540 + 0.4 * (user_earnings - 50270)
+    elif user_earnings > 125140:
         print("--- Additional rate Band ---")
-        tax_pay = (7540 + 29948) + 0.45 * (x - 125140)
-        return tax_pay
-
-# Main loop for the application which
-# takes the user input and displays the
-# amount of tax you would need to pay.
+        tax_payable = (7540 + 29948) + 0.45 * (user_earnings - 125140)
+    return tax_payable
 
 
-def main():
+# Prints welcome message at the start of the program
+def print_welcome_msg():
     print("-------------------------------------------")
     print("    WELCOME TO THE TAX CALCULATOR (UK)")
     print("APPLICATION THAT CALCULATES YOUR ANNUAL TAX")
     print("-------------------------------------------")
 
+
+# Takes user name and runs validations
+def take_user_name_input():
     name = input("Please enter your Name: ")
-    text_one = ", you would be paying: £"
-    text_two = "TAX this year"
+    if len(name) > 2 and name.replace(" ", "").isalpha():
+        return name
+    else:
+        print("The name you entered is invalid. Enter a valid \n")
+        return take_user_name_input()
+
+
+# Main loop for the application which
+# takes the user input and displays the
+# amount of tax you would need to pay.
+def main():
+    print_welcome_msg()
+
+    name = take_user_name_input()
+
     while True:
-        user_earn = input("How much you earn a year: £")
+        text_one = ", you would be paying: £"
+        text_two = "TAX this year"
+        user_earnings = input("How much you earn a year: £")
         # This is an input handler. If the value is a float or
-        # a numerical number it will accept it. But if a letter
+        # a numerical number it will accept it. But if it has alphabets
         # or special character has been entered then the
         # application would not accept it and return an error message.
         try:
-            float()
-            user_earn = float(user_earn)
-            print(name, text_one, "%.2f" % calculate(user_earn), text_two)
+            user_earnings = float(user_earnings)
+            if user_earnings < 0:
+                raise ValueError
+            tax_payable = calculate(user_earnings)
+            print(name, text_one, "%.2f" % tax_payable, text_two)
         except ValueError:
-            if user_earn.isnumeric():
-                user_earn = int(user_earn)
-                print(name, text_one, "%.2f" % calculate(user_earn), text_two)
-            else:
-                print("Insert a correct value to calculate your annual Tax!!!")
+            print("Insert a correct value to calculate your annual Tax!!!")
 
 
 main()
